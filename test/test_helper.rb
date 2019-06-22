1153b976b4e487a66d6f0dd21fcd8001fec39565
cineron <cineron+github@gmail.com>
2019-06-22 18:36:46 -0500

    Adds Guardfile and configures files for Testing.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+#Added per section 3.7 of https://3rd-edition.railstutorial.org/book/static_pages#sec-minitest_reporters
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
